0dffa56a1532092f761c31a9ed8fba52ecdc7ecb
unknown <djordjekovacevicsocials@gmail.com>
2023-10-10 14:56:43 +0200

    Added react router and partly implemented it on Content page.

--- src/Pages/Components/HomePage/Content.jsx
+++ src/Pages/Components/HomePage/Content.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 export default function Content() {
   return (
     <div className="w-2/4">
-      <p>Section2</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="Home" />
+          <Route path="Cooking" />
+          <Route path="Chemistry" />
+          <Route path="Mechanics" />
+          <Route path="Art" />
+          <Route path="Woodworking" />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
